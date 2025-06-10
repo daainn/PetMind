0fe3bb54518633707cc342b10999099b79ed6910
doyeon158 <183298259+doyeon158@users.noreply.github.com>
2025-06-10 10:36:26 +0900

    주석 제거

--- web/dogs/views.py
+++ web/dogs/views.py
@@ -61,30 +61,23 @@ def dog_info_join_view(request):
     
 @require_POST
 def delete_dog_profile(request, dog_id):
-    # 기존 코드 : 삭제 직후 기준으로 dog 개수를 확인함
-    # 따라서 => (삭제가 프로필 추가 페이지 존재하기 때문에 등록인지, 수정인지 구분 못 함)
-    # 수정 코드 : 삭제 이전 dog 개수 확인
 
-    user = get_logged_in_user(request)  # 유저 정보 확인
-    if not user:    # 로그인 안되는 경우
+    user = get_logged_in_user(request)
+    if not user:
         return redirect('user:home')
-    
-    # 삭제할 반려견 아이디 찾음
+
     try:
         dog = DogProfile.objects.get(id=dog_id, user=user)
     except DogProfile.DoesNotExist:
         return redirect('user:home')
-    
-    # 삭제 전 반려견 리스트 확인
-    all_dogs = DogProfile.objects.filter(user=user).order_by('created_at')
-    total_dog_count = all_dogs.count()  # 반려견 수 확인
 
-    dog.delete()  # 삭제 처리
+    all_dogs = DogProfile.objects.filter(user=user).order_by('created_at')
+    total_dog_count = all_dogs.count()
+
+    dog.delete()
 
     if total_dog_count == 1:
-        # 마지막 한 마리를 삭제한 경우
         return redirect('/dogs/dog_info/join/?mode=add')
     else:
-        # 여러 마리 중 하나만 삭제한 경우
         latest_dog = DogProfile.objects.filter(user=user).order_by('created_at').last()
         return redirect('chat:chat_member', dog_id=latest_dog.id)
