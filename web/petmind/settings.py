ec2d312da500574b804910e036af77c07973328b
Dain Lee <2ekdlsdl@gmail.com>
2025-06-11 20:07:13 +0900

    fix: s3

--- web/petmind/settings.py
+++ web/petmind/settings.py
@@ -114,7 +114,13 @@ AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_REGION_NAME = config('AWS_S3_REGION_NAME', default='ap-northeast-2')  # 서울
 AWS_QUERYSTRING_AUTH = False  # presigned url querystring 숨김
-MEDIA_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/"
+# MEDIA_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/"
+
+AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com"
+AWS_DEFAULT_ACL      = 'public-read'
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+MEDIA_URL            = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
 
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
