d5f39c6f304a5cc582241e2321b24d5fe9515544
Dain Lee <2ekdlsdl@gmail.com>
2025-06-12 10:29:23 +0900

    fix: s3 root

--- web/petmind/settings.py
+++ web/petmind/settings.py
@@ -118,9 +118,7 @@ AWS_QUERYSTRING_AUTH = False  # presigned url querystring 숨김
 
 AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com"
 AWS_DEFAULT_ACL      = 'public-read'
-
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-MEDIA_URL            = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
+MEDIA_URL            = f"https://{AWS_S3_CUSTOM_DOMAIN}/"
 
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
