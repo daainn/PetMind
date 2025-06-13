8fff2bdcebafc8a83dd2e571604e20ae876657ed
Dain Lee <2ekdlsdl@gmail.com>
2025-06-13 09:39:13 +0900

    fix: docker yml

--- web/chat/views.py
+++ web/chat/views.py
@@ -403,7 +403,7 @@ def get_chat_history(chat):
 
 def call_runpod_api(message, dog_info):
     try:
-        api_url = "http://38.128.232.8:41215/chat"
+        api_url = "http://69.48.159.14:21878/chat"
         payload = {
             "message": message,
             "dog_info": dog_info
